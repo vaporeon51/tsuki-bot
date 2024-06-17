2d50febbf95e21a2f5bbfe96199707fa6681faf6
vaporeon51 <latemp2@outlook.com>
2024-06-17 08:13:11 -0400

    decrease sec

--- src/config/constants.py
+++ src/config/constants.py
@@ -3,7 +3,7 @@ DOWNVOTE_EMOTE = "🔽"
 REPORT_EMOTE = "⚠️"
 
 # Number of seconds to wait before updating db based on reactions
-REACT_WAIT_SEC = 5 * 60
+REACT_WAIT_SEC = 3 * 60
 
 # Number of reports to eliminate a role + link combination from being shown
 REPORT_THRESHOLD = 5
