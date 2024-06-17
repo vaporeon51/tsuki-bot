3b75b2f82d416e527d09aa3793dcfa2f16c45803
Lemon <soljinbae1228@gmail.com>
2024-06-17 17:00:28 +0000

    sql evolution

--- scripts/sql/table_updates2.sql
+++ scripts/sql/table_updates2.sql
@@ -4,4 +4,4 @@ ADD tsv_group_name tsvector;
 
 UPDATE role_info
 SET tsv_member_name = to_tsvector('english', member_name),
-    tsv_group_name = to_tsvector('english', group_name);
+    tsv_group_name = to_tsvector('english', regexp_replace(group_name, '[^a-zA-Z0-9]+', '', 'g'));
--- src/db/utils.py
+++ src/db/utils.py
@@ -8,26 +8,34 @@ CONN_DICT = psycopg.conninfo.conninfo_to_dict(DATABASE_URL)
 
 def find_closest_role(query: str) -> str | None:
     """Given a query find the best role that match with the query."""
-    search_term = query.replace(" ", " | ")
     with psycopg.connect(**CONN_DICT) as conn:
         with conn.cursor() as cur:
             cur.execute(
                 """
                 WITH query AS (
-                    SELECT to_tsquery('english', %s) AS search_terms
+                    SELECT to_tsquery('english',
+                            regexp_replace(
+                                regexp_replace(%s, '\s+', ' | ', 'g'),
+                                '[^a-zA-Z0-9|\s]+', '', 'g')) AS search_terms
+                ),
+                ranked_roles AS (
+                    SELECT role_id, member_name, group_name,
+                        ts_rank_cd(tsv_member_name, query.search_terms) +
+                        ts_rank_cd(tsv_group_name, query.search_terms) AS r,
+                        rank() OVER (ORDER BY ts_rank_cd(tsv_member_name, query.search_terms) +
+                        ts_rank_cd(tsv_group_name, query.search_terms) DESC) AS rank
+                    FROM role_info, query
+                    WHERE
+                        tsv_member_name @@ query.search_terms
+                        OR
+                        tsv_group_name @@ query.search_terms
                 )
-                SELECT role_id,
-                    ts_rank_cd(tsv_member_name, query.search_terms) +
-                    ts_rank_cd(tsv_group_name, query.search_terms) AS rank
-                FROM role_info
-                WHERE
-                    (tsv_member_name @@ query.search_terms)
-                    AND
-                    (tsv_group_name @@ query.search_terms)
-                ORDER BY rank DESC
-                LIMIT 1
+                SELECT role_id, member_name, group_name, rank, r
+                FROM ranked_roles
+                WHERE rank = 1
+                ORDER BY random(); 
             """,
-                (search_term),
+                (query),
             )
 
             # Fetch the first result
