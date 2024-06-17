420a4789ae882a7719e08e312fceacc7e04578ef
vaporeon51 <157034886+vaporeon51@users.noreply.github.com>
2024-06-17 13:13:16 -0400

    Decrease the sampling exponent

--- src/config/constants.py
+++ src/config/constants.py
@@ -8,5 +8,5 @@ REACT_WAIT_SEC = 3 * 60
 # Number of reports to eliminate a role + link combination from being shown
 REPORT_THRESHOLD = 5
 
-# 5x net upvotes translates to 2x the probability so log(2)/log(5) = 0.43
-SAMPLING_EXPONENT = 0.43067655807
+# 10x net upvotes translates to 2x the probability so log(2)/log(10) = 0.30
+SAMPLING_EXPONENT = 0.30102999566
