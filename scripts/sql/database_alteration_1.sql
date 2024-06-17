ef8f9b3fe0ff0204d14db808feb054bb9dbc962b
Lemon <soljinbae1228@gmail.com>
2024-06-17 15:53:18 +0000

    dupe file

--- scripts/sql/database_alteration_1.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE role_info
-ADD COLUMN tsv_member_name tsvector,
-ADD COLUMN tsv_group_name tsvector;
-
-UPDATE role_info
-SET tsv_member_name = to_tsvector('english', member_name),
-    tsv_group_name = to_tsvector('english', group_name);
