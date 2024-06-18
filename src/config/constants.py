e231dabbb5a6f9be1cd08a5becaac01dedeafec8
vaporeon51 <latemp2@outlook.com>
2024-06-18 17:10:25 -0400

    add more diversity

--- src/config/constants.py
+++ src/config/constants.py
@@ -7,11 +7,11 @@ REACT_WAIT_SEC = 3 * 60
 # Number of reports to eliminate a role + link combination from being shown
 REPORT_THRESHOLD = 5
 
-# 10x net upvotes translates to 2x the probability so log(2)/log(10) = 0.30
-SAMPLING_EXPONENT = 0.30102999566
+# 20x net upvotes translates to 2x the probability so log(2)/log(20)
+SAMPLING_EXPONENT = 0.23137821316
 
 # Cap on the contribution of initial reactions from kpf to weight
-INITIAL_REACT_CAP = 200
+INITIAL_REACT_CAP = 100
 
 # Size of most recently sent URLs queue to prevent duplicates
 RECENTLY_SENT_QUEUE_SIZE = 10
