a339986c0e8ad6e602296c88356c6ae836d3dd1e
vaporeon51 <latemp2@outlook.com>
2024-07-15 20:38:34 -0400

    try https

--- src/reddit_feeds.py
+++ src/reddit_feeds.py
@@ -26,7 +26,7 @@ def get_latest_posts() -> list[dict]:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
-    resp = requests.get("http://www.reddit.com/r/kpopfap/new.json?sort=new", headers=headers, timeout=5)
+    resp = requests.get("https://www.reddit.com/r/kpopfap/new.json?sort=new", headers=headers, timeout=5)
     return resp.json()
 
 
